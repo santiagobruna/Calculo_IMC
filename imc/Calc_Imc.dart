99de6f529a73c1a5595b323f12b41002a3e64dfd
bruna_carla <brsant81@gmail.com>
2022-10-25 19:15:51 -0300

    passando funções com parâmetro e tipagem

--- imc/Calc_Imc.dart
+++ imc/Calc_Imc.dart
@@ -1,27 +1,35 @@
 import 'dart:io';
+
 main() {
+  calculoImc();
+}
+
+calculoImc() {
   print("Digite o seu peso: ");
-  var input = stdin.readLineSync();
-  var peso = int.parse(input!);
+  String textPeso = stdin.readLineSync()!;
+  int peso = int.parse(textPeso);
 
   print("Digite a sua altura: ");
-  var input2 = stdin.readLineSync();
-  var altura = double.parse(input2!);
+  String textAltura = stdin.readLineSync()!;
+  double altura = double.parse(textAltura);
 
-  var calc = peso / (altura * altura);
+  double calc = peso / (altura * altura);
 
-  if (calc < 18.5 ) {
+  imprimirResultado(calc);
+}
+
+imprimirResultado(double calc) {
+  if (calc < 18.5) {
     print("Abaixo do Peso");
   } else if (calc > 18.5 && calc < 24.9) {
     print("Peso Normal");
   } else if (calc > 25 && calc < 29.9) {
     print("Sobrepeso");
-  } else if(calc > 30 && calc < 34.9){
+  } else if (calc > 30 && calc < 34.9) {
     print("Obesidade Grau 1");
-  }else if(calc > 35 && calc < 39.9){
+  } else if (calc > 35 && calc < 39.9) {
     print("Obesidade Grau 2");
-  }else{
+  } else {
     print("Obesidade Grau 3");
   }
-  
 }
